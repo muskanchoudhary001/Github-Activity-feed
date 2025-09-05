84a8f8f74d575a38aa53094b4b23b76371d32a81
Muskan Choudhary <81254978+muskanchoudhary001@users.noreply.github.com>
2025-09-05 22:01:52 +0530

    `Removed package-lock.json, package.json, postcss.config.js, src/config.js, src/index.js, src/services/services.js, and tailwind.config.js files.`

--- src/config.js
+++
@@ -1,6 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const PORT = process.env.PORT || 5000;
-export const GITHUB_USERNAME = process.env.GITHUB_USERNAME || "muskanchoudhary001";
